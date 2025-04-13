37bdfa965496f1cac43e47902e12f3df71855934
Bas Maas <github.hunter841@passmail.net>
2025-04-13 11:36:41 +0200

    Deleted clusters/*

--- .mise/tasks/lint.sh
+++ .mise/tasks/lint.sh
@@ -7,7 +7,7 @@ yamllint . -c "${ROOT_DIR}/.lint/yamllint.yaml"
 
 echo "Linting shell scripts..."
 # shellcheck disable=SC2046
-shellcheck $(find . -name "*.sh")
+shellcheck $(find . -name "*.sh" ! -path "./.ignore/*")
 
 echo "Validating Terraform code..."
 terraform -chdir="${TERRAFORM_DIR}" validate
