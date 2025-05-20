70be1c0e7fe9d598760f9f2259e68d2ed3efbb23
Bas Maas <github.hunter841@passmail.net>
2025-05-20 09:47:44 +0000

    Update argocd bootstrap config

--- .mise/tasks/bootstrap.sh
+++ .mise/tasks/bootstrap.sh
@@ -21,7 +21,7 @@ helm secrets upgrade \
     --namespace argocd \
     --create-namespace \
     --values "${ARGOCD_DIR}/values.yaml" \
-    --values "${ARGOCD_DIR}/secret.values.dec.yaml" \
+    --values "${ARGOCD_DIR}/secret.values.enc.yaml" \
     --wait
 
 kubectl apply -f "${BOOTSTRAP_DIR}/bootstrap.yaml"
