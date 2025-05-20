d48dffce5b2016a7ada26f46c6488f689a6d332a
Bas Maas <github.hunter841@passmail.net>
2025-05-20 21:43:34 +0200

    Added linting of Helm charts

--- .mise/tasks/lint.sh
+++ .mise/tasks/lint.sh
@@ -11,3 +11,9 @@ shellcheck $(find . -name "*.sh" ! -path "./.ignore/*")
 
 echo "Validating Terraform code..."
 terraform -chdir="${TERRAFORM_DIR}" validate
+
+echo "Linting Helm charts..."
+find "${ROOT_DIR}" -name "Chart.yaml" -exec dirname {} \; | while read -r chart_dir; do
+    echo "Linting chart in ${chart_dir}..."
+    helm lint "${chart_dir}"
+done
