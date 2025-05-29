2504af04c25a1edb3c5413054018b9335ea5182c
Bas Maas <github.hunter841@passmail.net>
2025-05-29 19:19:04 +0000

    Updated mise init script

--- .mise/tasks/init.sh
+++ .mise/tasks/init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #MISE description="Init the repo"
 set -e
+
 echo "Initializing repository..."
 
 mise run decrypt .env.enc.json
@@ -15,5 +16,16 @@ terraform -chdir="${TERRAFORM_DIR}" init
 # Set githooks
 echo "Setting githooks"
 git config --local core.hooksPath .githooks/
+
+# Get kubeconfig
+state_exists=$(terraform -chdir="${TERRAFORM_DIR}" state list >/dev/null 2>&1; echo $?)
+if [ "${state_exists}" -eq 0 ]; then
+    test -f "${KUBECONFIG}" || mise run kubeconfig
+fi
+
+# Get talosconfig
+if [ "${state_exists}" -eq 0 ]; then
+    test -f "${TALOSCONFIG}" || mise run talosconfig
+fi
 
 echo "Repository initialized"
