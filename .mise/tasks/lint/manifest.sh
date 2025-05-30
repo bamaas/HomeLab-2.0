39b9e6d9c166cc658dce48d09c05f162b79d82de
Bas Maas <github.hunter841@passmail.net>
2025-05-30 20:37:51 +0000

    Run kubeconform in strict mode

--- .mise/tasks/lint/manifest.sh
+++ .mise/tasks/lint/manifest.sh
@@ -26,6 +26,6 @@ if [ -z "$SCHEMA_VALIDATION" ]; then
 else
   echo "Rendering chart in ${CHART_DIR} and validating manifests."
   helm secrets template --release-name "${namespace}" -n "${namespace}" "${CHART_DIR}" -f "${CHART_DIR}/values.yaml" -f "${CHART_DIR}/values.enc.yaml" | \
-    kubeconform -summary -ignore-missing-schemas -summary -kubernetes-version 1.31.0
+    kubeconform -summary -ignore-missing-schemas -summary -strict -kubernetes-version 1.31.0
   echo "Chart in ${CHART_DIR} is valid"
 fi
